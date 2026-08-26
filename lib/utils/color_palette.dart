c6d009e74beca997d6c43166a74bd0b727de8d9d
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-26 21:37:02 +0100

    Create lib/utils/color_palette.dart

---
+++ lib/utils/color_palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryDark = Color(0xFF0F172A);
+  static const Color cardDark = Color(0xFF1E293B);
+  static const Color accentIndigo = Color(0xFF4F46E5);
+  static const Color accentAmber = Color(0xFFF59E0B);
+  static const Color glowPurple = Color(0xFF8B5CF6);
+  static const Color textWhite = Colors.white;
+  static const Color textMuted = Colors.white70;
+}
