31a6f92037b1c656cee8e0b342592e8575970020
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-27 10:59:25 +0100

    Create lib/views/splash_view.dart

---
+++ lib/views/splash_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF0F172A),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.auto_awesome, size: 64, color: Colors.indigoAccent),
+            SizedBox(height: 16),
+            Text(
+              'Mind Spark Elite',
+              style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            CircularProgressIndicator(color: Colors.indigoAccent),
+          ],
+        ),
+      ),
+    );
+  }
+}
