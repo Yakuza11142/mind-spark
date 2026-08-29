e931ee03d6dbe6fe3a1de5449f5456643ba16cd0
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-29 13:12:00 +0100

    Update gradlew.bat

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,2 +1,34 @@
-@rem Minimal redirect executor
-@java -jar "%~dp0gradle\wrapper\gradle-wrapper.jar" %*
+@rem Official Gradle Wrapper execution script for Windows
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem  Gradle startup script for Windows
+@rem ##########################################################################
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+if "%JAVA_HOME%"=="" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto badJavaHome
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+goto execute
+
+:noJavaHome
+set JAVA_EXE=java
+%JAVA_EXE% -version >NUL 2>&1
+if %ERRORLEVEL% equ 0 goto execute
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+goto fail
+
+:badJavaHome
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+goto fail
+
+:execute
+"%JAVA_EXE%" "-Xmx64m" "-Xms64m" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:fail
+exit /b 1
