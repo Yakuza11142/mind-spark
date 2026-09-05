94768fa892ec1c4cf0b53c36d47549fe8353afb3
Yakuza11142 <yakubupeter785@gmail.com>
2026-09-05 20:44:21 +0100

    Create lib/screens/intro_screen_stub.dart

---
+++ lib/screens/intro_screen_stub.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NativeWebIntroVideo extends StatelessWidget {
+  const NativeWebIntroVideo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Standard black background layout placeholder for mobile engines
+    return const SizedBox.shrink();
+  }
+}
